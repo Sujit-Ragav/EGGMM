713cdd067f01929b6dc22ff2ba8188a73b7d0e47
Sujit Ragav M <83977702+Sujit-Ragav@users.noreply.github.com>
2024-03-14 09:30:38 +0530

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -7,11 +7,7 @@ class BaseNetwork(nn.Module):
         super(BaseNetwork, self).__init__()
 
     def init_weights(self, init_type='normal', gain=0.02):
-        '''
-        initialize network's weights
-        init_type: normal | xavier | kaiming | orthogonal
-        https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/9451e70673400885567d08a9e97ade2524c700d0/models/networks.py#L39
-        '''
+       
 
         def init_func(m):
             classname = m.__class__.__name__
